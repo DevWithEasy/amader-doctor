faf6763b5d7d94b8e8c5ed5d73d08cd0259dc84d
DevWithEasy <devwitheasy@gmail.com>
2024-04-08 16:56:52 +0600

    code server

--- app/appointment/find/page.js
+++ app/appointment/find/page.js
@@ -1,11 +1,19 @@
-'use client'
-import {useSearchParams} from 'next/navigation'
+import api_url from "@/app/_utils/apiurl";
 
-export default function Page() {
-    const searchParams = useSearchParams()
-    const specialization = searchParams.get('specialization')
-    const day = searchParams.get('day')
-    console.log(specialization,day)
+async function getData(specialization,day) {
+    const randomParam = Math.random().toString(36).substring(7);
+    const res = await fetch(`${api_url}/api/doctor/find?specialization=${specialization}&day=${day}&?${randomParam}`);
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+    return res.json();
+}
+
+export default async function Page({ searchParams }) {
+    const { specialization, day } = searchParams
+    const data = await getData(specialization, day)
+    console.log(data)
     return (
         <div>Find</div>
     )
