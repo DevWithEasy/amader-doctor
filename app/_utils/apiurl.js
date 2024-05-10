72ef5cb74b396f8485b251de30d075f2eec3558a
DevWithEasy <devwitheasy@gmail.com>
2024-05-10 18:12:34 +0600

    api url change

--- app/_utils/apiurl.js
+++ app/_utils/apiurl.js
@@ -1,3 +1,3 @@
-const api_url = 'http://localhost:8080'
+const api_url = process.env.NODE_ENV === 'production' ?  'https://amaderdoctor.onrender.com'  : 'http://localhost:8080'
 
 export default api_url;
